de2efd6aac90715ee0491ea28ec497e160f69b81
K-iheb <mohamed.elkhedhri@groupe-esigelec.org>
2020-02-13 16:04:00 +0100

    unity3d demo

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	private string moveInputAxis = "Vertical";
+	private string turnInputAxis = "Horizontal";
+	public float rotationRate = 360;
+	public float moveSpeed = 2;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float moveAxis = Input.GetAxis(moveInputAxis);
+		float turnAxis = Input.GetAxis(turnInputAxis);
+	}
+private  void ApplyInput(float)
+	private void Move(float input)
+	{
+		
+	}
+	private void Turn(float input)
+	{
+		
+	}
+}
---
+++ Assets/script/SphereScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+	private GameObject sphere;
+	// Use this for initialization
+	void Start ()
+	{
+		sphere =GameObject.Find("Sphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sphere.transform.Translate(1,0,0);
+	}
+}
